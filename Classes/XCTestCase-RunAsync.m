22f0ea6382410245d3f0c8504ec45a72fb57c611
azu <azuciao@gmail.com>
2014-06-05 16:03:22 +0900

    rename file

--- Classes/XCTestCase-RunAsync.m
+++ Classes/XCTestCase-RunAsync.m
@@ -3,7 +3,7 @@
 //
 
 
-#import "XCTestCase+RunAsync.h"
+#import "XCTestCase-RunAsync.h"
 #import "NSRunLoop+PerformBlock.h"
 
 
--- Example/ExampleTests/ExampleTests.m
+++ Example/ExampleTests/ExampleTests.m
@@ -7,7 +7,7 @@
 //
 
 #import <XCTest/XCTest.h>
-#import "XCTestCase+RunAsync.h"
+#import "XCTestCase-RunAsync.h"
 
 @interface ExampleTests : XCTestCase
 
